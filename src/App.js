6635564d1fc902de695db12ce74cff9dac036693
Coskun Cabuk <coskuncabuk@Coskuns-MacBook-Air.local>
2023-02-05 21:52:27 +0000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex">
+      <div className="box">1</div>
+      <div className="box">2</div>
+      <div className="box">3</div>
+      <div className="box">4</div>
+      <div className="box">5</div>
+    </div>
+  );
+}
+
+export default App;
